de7647ca2d068b8fcf6021368353b525bdfe9cac
Nick Elliott <nickmartinelliott@gmail.com>
2021-02-22 22:01:06 -0500

    Updated most service's button text to better reflect the click through option. Added and updated routes including: index, services, orders and login.

--- src/App.js
+++ src/App.js
@@ -4,14 +4,18 @@ import './App.css';
 import './normalize.css';
 import './style.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/js/dist/collapse';
+import 'bootstrap/dist/js/bootstrap.min.js';
 import Header from './components/Header';
 import OrdersMain from './components/OrdersMain';
 import ServicesMain from './components/ServicesMain';
+import OrderMainPendingItems from './components/OrderMainPendingItems';
+import OrderPlace from './components/OrderPlace';
+import Login from './components/Login';
 
 
 class App extends Component {
   state =  { 
+    //orders array prevents error caused by lack of conditional checking if orders are being served in OrderMainPendingItems component
     orders: [],
     navOptions: [
       'home', 
@@ -27,31 +31,31 @@ class App extends Component {
     loading: false,
     services: [
       {
-        type: "Build new screens",
+        type: "Build New Screens",
         desc: "Choose the color of your: frame, screen, hardware, etc.",
         imgSrc: "border-outer.svg",
-        btn: "Build",
+        btn: "Build New",
         href: "bScreens"
       },
       {
-        type: "Build new windows",
+        type: "Build New Windows",
         desc: "Choose the color of your: frame, screen, hardware, etc.",
         imgSrc: "border-all.svg",
-        btn: "Build",
+        btn: "Build New",
         href: "bWindows"
       },
       {
-        type: "Restore old screens",
+        type: "Restore Old Screens",
         desc: "Bring your house to life by repairing those old, dirty, torn screens.",
         imgSrc: "layers-fill.svg",
-        btn: "Restore",
+        btn: "Restore Old",
         href: "rScreens"
       },
       {
-        type: "Restore old windows",
+        type: "Restore Old Windows",
         desc: "Defeat the winter's cold air and revamp your old storm windows.",
         imgSrc: "layers-half.svg",
-        btn: "Restore",
+        btn: "Restore Old",
         href: "rWindows"
       },
       {
@@ -78,13 +82,31 @@ class App extends Component {
 
     return(
       <BrowserRouter>
-          <div height="70px" />
+          <div height="70px" width="100%" />
           <div>
               <Header paths={this.state.navOptions} search={this.getPhotos} />
-              <ServicesMain services={this.state.services}/>
-              <OrdersMain orders={this.state.orders}/>
+              {/* <ServicesMain services={this.state.services}/>
+              <OrdersMain orders={this.state.orders}/> */}
               <Switch>
-                <Route exact path="/" /> 
+                <Route exact path="/" render={ () => (           
+                    <> 
+                      <ServicesMain services={this.state.services}/>
+                      <OrdersMain orders={this.state.orders}/>
+                    </>
+                  )}
+                /> 
+                <Route exact path="/services" render={ () => (           
+                    <> 
+                      <ServicesMain services={this.state.services}/>
+                    </>
+                  )}
+                /> 
+                <Route exact path="/orders" render={ () => (           
+                    <> 
+                      <OrderPlace services={this.state.services} cart={this.state.cart}/>
+                    </>
+                  )}
+                /> 
                 <Route  path="/search/:in" 
                         render={ props => (            
                           (this.state.loading)
@@ -92,6 +114,7 @@ class App extends Component {
                             : <OrdersMain />
                         )} 
                 />
+                <Route path="/login" component={Login} />
               </Switch>
           </div>
         </BrowserRouter>
