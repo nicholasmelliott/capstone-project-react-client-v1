377088c4694677e10adef6218a01128dc9c40041
Nick Elliott <nickmartinelliott@gmail.com>
2021-02-28 10:23:33 -0500

    Updated Services styling: Made service cards background a transparent gradient, Added highlighting border when card is hovered over, fixed service cards being hidden by the navbar. Also removed alert from top of services.

--- src/components/ServicesMain.js
+++ src/components/ServicesMain.js
@@ -2,22 +2,17 @@ import React from 'react';
 
 const ServicesMain = ({ services , loading }) => (
 	<div>
-		<div class="alert alert-dark alert-dismissible fade show ml-xl-4 ml-xl-4 text-center services-alert" role="alert"><strong>Needing a service? </strong>Click a service below to learn more and place an order.
-      <button class="close" type="button" data-dismiss="alert" aria-label="Close">
-          <span aria-hidden="true">&times; </span>
-      </button>
-    </div>
     <div class="d-none d-md-block services-main">
         <div class="d-flex">
           {services.map((service)=>{
             return(
-              <div class="card col" width="18rem">
-               <img class="card-img-top mt-2" src={service.imgSrc} style={{fill: "#EBEBEB"}} alt="Card image cap" />
+              <div class="card col" width="18rem" style={{backgroundColor: 'rgba(255,255,255,0)'}}>
+               <img class="card-img-top mt-2" src={service.imgSrc} alt="Card image cap" />
                <div class="card-body pl-0 pr-0">
                 <h5 class="card-title">{service.type}</h5>
                 <p class="card-text">{service.desc}</p>
                </div>
-               <div class="card-footer text-muted service-card-footer"><a class="btn btn-outline-secondary font-weight-bold btn-md btn-block footer" href="/orders">{service.btn}</a></div>
+               <div class="card-footer text-muted service-card-footer"><a class="btn btn-outline-secondary font-weight-bold btn-md btn-block footer service-btn" href="/orders">{service.btn}</a></div>
             </div>
             );
           })} 
@@ -30,7 +25,7 @@ const ServicesMain = ({ services , loading }) => (
             <div class="col-4 content-justify-center mt-2">
               <a href="/orders">
                 <img src={service.imgSrc} alt="Card image cap" width="80px" height="auto" />
-                <p>
+                <p style={{color: '#f8f9fa'}}>
                   {service.type}
                 </p>
               </a>  
