658ac25bbdc93b6c164923ee1f872ef9b5ec25cf
Nick Elliott <nickmartinelliott@gmail.com>
2021-04-04 10:00:47 -0400

    Temperary update to disable Host Check.

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,9 +1,13 @@
 module.exports = {
-    ...
     plugins: [
         new webpack.ProvidePlugin({
            $: "jquery",
            jQuery: "jquery"
        })
-    ]
+    ],
+
+    devServer: {
+        compress: true,
+        disableHostCheck: true
+     }      
 };
