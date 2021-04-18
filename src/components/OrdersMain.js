bbf90b30b7d205b9ea60f83c55991d5c8bb0beb2
Nick Elliott <nickmartinelliott@gmail.com>
2021-04-17 20:23:59 -0400

    Removed odd, responsive spacing between Completed and Pending Orders divs.

--- src/components/OrdersMain.js
+++ src/components/OrdersMain.js
@@ -3,19 +3,19 @@ import OrderMainCompleteItems from './OrderMainCompleteItems';
 import OrderMainPendingItems from './OrderMainPendingItems';
 
 const OrdersMain = ({ orders, loading }) => (
-	<div>
+	<div class="orders-main p-md-4">
         <div class="alert alert-dark alert-dismissible fade show mt-2 ml-xl-4 mr-xl-4 text-center order-list-alert" role="alert"><strong>Looking to see if your order has been completed? </strong>Check the fields below.
           <button class="close" type="button" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;       </span></button>
         </div>
         <div class="d-none d-lg-block order-list-main">
           <div class="row">
-            <div class="col pr-0 ml-xl-4 mr-xl-2">
+            <div class="col pr-0">
               <div class="complete-header">Completed Orders<img class="ml-2" src="check2-circle.svg"/></div>
               <div class="orders-large-body"> 
                 <OrderMainCompleteItems orders={orders} />
               </div>
             </div>
-            <div class="col pl-0 ml-xl-2 mr-xl-4">  
+            <div class="col pl-0">  
               <div class="pending-header">Pending Orders<img class="ml-2" src="circle.svg" width="14px" height="14px"/></div>
               <div class="orders-large-body"> 
                 <OrderMainPendingItems orders={orders} />
