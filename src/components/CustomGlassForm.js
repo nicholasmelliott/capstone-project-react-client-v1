40bd7c19f427e3b9cee8f3dda95709d047becd6a
nicholasmelliott <30277860+nicholasmelliott@users.noreply.github.com>
2023-07-09 18:39:09 -0400

    Refactor class attribute to className for consistent usage in the codebase.

--- src/components/CustomGlassForm.js
+++ src/components/CustomGlassForm.js
@@ -3,33 +3,33 @@ import React from 'react';
 const prod = "customGlass"
 const CustomGlassForm = ({ services, change, quantityUI, dimensionUI}) => (
 	<div> 
-    <div class="row">
-      <div class="col-8">
-        <h2 class="font-weight-bold">Cut <br/> Custom <br/> Glass</h2>
+    <div className="row">
+      <div className="col-8">
+        <h2 className="font-weight-bold">Cut <br/> Custom <br/> Glass</h2>
       </div>
-      <div class="col-4">
-        <img src={services[4].imgSrc} width="80%" class="img-form"/>
+      <div className="col-4">
+        <img src={services[4].imgSrc} width="80%" className="img-form"/>
       </div>
     </div>
     <h6>Quantity</h6>      
     {quantityUI(prod, "quantity")}
     <hr/>
     <h6>Dimensions</h6>
-    <div class="row" style={{paddingLeft: 15 + "px", paddingRight: 15 + "px"}}>
-      <label class="form-row ml-1" for="width">Please enter the glass's dimensions (in inches):</label>
+    <div className="row" style={{paddingLeft: 15 + "px", paddingRight: 15 + "px"}}>
+      <label className="form-row ml-1" for="width">Please enter the glass's dimensions (in inches):</label>
     </div>
-    <div class="form-row mt-2">
+    <div className="form-row mt-2">
       {dimensionUI(prod,'width')}
     </div>
-    <div class="form-row mt-2">
+    <div className="form-row mt-2">
       {dimensionUI(prod,'height')}
     </div>
     <hr/>
     <h6>Material</h6>
-    <div class="row">     
-      <div class="form-group col-12"> 
-        <label class="mt-2" for="rWMaterial">Please select the material:</label>
-        <select class="form-control" id="WMaterial" name="material" onChange={change} required>
+    <div className="row">     
+      <div className="form-group col-12"> 
+        <label className="mt-2" for="rWMaterial">Please select the material:</label>
+        <select className="form-control" id="WMaterial" name="material" onChange={change} required>
           <option value="">...</option>
           <option value="SSAnealGlass">Single Strength Annealed Glass</option>
           <option value="DSAnealGlass">Double Strength Annealed Glass</option>
--- src/components/RestoreScreensForm.js
+++ src/components/RestoreScreensForm.js
@@ -3,47 +3,47 @@ import React from 'react';
 const prod = "restoreScreen"
 const RestoreScreensForm = ({ services, change, quantityUI, dimensionUI, hardwareUI}) => (
 	<div> 
-    <div class="row">
-      <div class="col-8">
-        <h2 class="font-weight-bold">Restore <br/> Old <br/> Screen(s)</h2>
+    <div className="row">
+      <div className="col-8">
+        <h2 className="font-weight-bold">Restore <br/> Old <br/> Screen(s)</h2>
       </div>
-      <div class="col-4">
-        <img src={services[2].imgSrc} width="80%" class="img-form"/>
+      <div className="col-4">
+        <img src={services[2].imgSrc} width="80%" className="img-form"/>
       </div>
     </div>
     <h6>Quantity</h6>      
     {quantityUI(prod, "quantity")}
     <hr/>
     <h6>Dimensions</h6>
-    <div class="row" style={{paddingLeft: 15 + "px", paddingRight: 15 + "px"}}>
-      <label class="form-row ml-1" for="width">Please enter the screen's dimensions (in inches):</label>
+    <div className="row" style={{paddingLeft: 15 + "px", paddingRight: 15 + "px"}}>
+      <label className="form-row ml-1" for="width">Please enter the screen's dimensions (in inches):</label>
     </div>
-    <div class="form-row mt-2">
+    <div className="form-row mt-2">
       {dimensionUI(prod,'width')}
     </div>
-    <div class="form-row mt-2">
+    <div className="form-row mt-2">
       {dimensionUI(prod,'height')}
     </div>
-    <div class="form-row mt-2">
+    <div className="form-row mt-2">
       {dimensionUI(prod, 'depth')}
     </div>
     <hr/>
     <h6>Frame</h6>
-    <div class="row">  
-      <div class="col-6">
-        <div class="form-group"> 
-          <label class="mt-2" for="rSFType">Please select the frame type:</label>
-          <select class="form-control" id="rSFType" name="frameType" onChange={change} required>
+    <div className="row">  
+      <div className="col-6">
+        <div className="form-group"> 
+          <label className="mt-2" for="rSFType">Please select the frame type:</label>
+          <select className="form-control" id="rSFType" name="frameType" onChange={change} required>
             <option value="">...</option>
             <option value="standard">Standard</option>
             <option value="storm">Storm</option>
           </select>
         </div>
       </div>
-      <div class="col-6">
-        <div class="form-group"> 
-          <label class="mt-2" for="rSFColor">Please select the frame color:</label>
-          <select class="form-control" id="rSFColor" name="frameColor" onChange={change} required>
+      <div className="col-6">
+        <div className="form-group"> 
+          <label className="mt-2" for="rSFColor">Please select the frame color:</label>
+          <select className="form-control" id="rSFColor" name="frameColor" onChange={change} required>
             <option value="">...</option>
             <option value="white">White</option>
             <option value="bronze">Bronze</option>
@@ -60,10 +60,10 @@ const RestoreScreensForm = ({ services, change, quantityUI, dimensionUI, hardwar
     {hardwareUI(prod, 'hardware')}
     <hr/>
     <h6>Screen</h6>
-    <div class="row">     
-      <div class="form-group col-12"> 
-        <label class="mt-2" for="rSType">Please select the screen color and material:</label>
-        <select class="form-control" id="rSType" name="sType" onChange={change} required>
+    <div className="row">     
+      <div className="form-group col-12"> 
+        <label className="mt-2" for="rSType">Please select the screen color and material:</label>
+        <select className="form-control" id="rSType" name="sType" onChange={change} required>
           <option value="">...</option>
           <option value="charFib">Charcoal Fiberglass</option>
           <option value="grayFib">Gray Fiberglass</option>
