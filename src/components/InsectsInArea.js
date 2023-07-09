1173bb0eb07f16a0bbfd849a5fe3544dcf3fbcfa
nicholasmelliott <30277860+nicholasmelliott@users.noreply.github.com>
2023-07-08 23:10:52 -0400

    Refactor class attribute to className for consistent usage in the codebase. Refactor for to htmlFor and tabindex to tabIndex.

--- src/components/InsectsInArea.js
+++ src/components/InsectsInArea.js
@@ -82,49 +82,49 @@ const InsectsInArea = ({ search, insects, submit, USState, loading, NatureServeC
         const informalTax = insect.informalTax;
         if(insect.photo != ""){
             return(
-                <img src={insect.photo} class={`${size}`} style={{borderRadius: 50 + "%"}}/>
+                <img src={insect.photo} className={`${size}`} style={{borderRadius: 50 + "%"}}/>
             );
         }else{
             if(insect.commonName){
                 if(/\bBees\b/.test(informalTax)){
                     return (
-                        <img src={BeeLogo} class={`${size}`}/> 
+                        <img src={BeeLogo} className={`${size}`}/> 
                     );
                 }else if(/\bSpiders\b/i.test(informalTax)){
                     return (
-                      <img src={SpiderLogo} class={`${size}`}/> 
+                      <img src={SpiderLogo} className={`${size}`}/> 
                     );
                 }else if(/\bBeetles\b/i.test(informalTax)){
                     return (
-                      <img src={BeetleLogo} class={`${size}`}/> 
+                      <img src={BeetleLogo} className={`${size}`}/> 
                     );
                 }else if(/\bSkippers\b/i.test(informalTax)){
                     return (
-                        <img src={ButterFlyLogo} class={`${size}`}/> 
+                        <img src={ButterFlyLogo} className={`${size}`}/> 
                     );
                 }else if(/\bMoths\b/i.test(informalTax)){
                     return (
-                        <img src={MothLogo} class={`${size}`}/> 
+                        <img src={MothLogo} className={`${size}`}/> 
                     );
                 }else if(/\bDragonflies\b/i.test(informalTax)){
                     return (
-                        <img src={DragonFlyLogo} class={`${size}`}/> 
+                        <img src={DragonFlyLogo} className={`${size}`}/> 
                     );
                 }else if(/\bAnts\b/i.test(informalTax)){
                     return (
-                        <img src={AntLogo} class={`${size}`}/> 
+                        <img src={AntLogo} className={`${size}`}/> 
                     );
                 }else if(/\bGrasshoppers\b/i.test(informalTax)){
                     return (
-                        <img src={GrassHopperLogo} class={`${size}`}/> 
+                        <img src={GrassHopperLogo} className={`${size}`}/> 
                     );
                 }else if(/\bFlies\b/i.test(informalTax)){
                     return (
-                        <img src={FlyLogo} class={`${size}`}/> 
+                        <img src={FlyLogo} className={`${size}`}/> 
                     );
                 }else if(/\bMayflies\b/i.test(informalTax)){
                     return (
-                        <img src={MayFlyLogo} class={`${size}`}/> 
+                        <img src={MayFlyLogo} className={`${size}`}/> 
                     );
                 }
                 // else{
@@ -137,22 +137,22 @@ const InsectsInArea = ({ search, insects, submit, USState, loading, NatureServeC
     };
     //Displays loading screen
     const loadingUI = 
-        <div class="d-flex justify-content-center pt-3" style={{backgroundColor: "white", width: 100 + "%", height: 100 + "px"}}>
-            <h5 class="ml-3">Loading</h5>
-            <div class="spinner-grow ml-1" role="status">
-                <span class="sr-only">Loading...</span>
+        <div className="d-flex justify-content-center pt-3" style={{backgroundColor: "white", width: 100 + "%", height: 100 + "px"}}>
+            <h5 className="ml-3">Loading</h5>
+            <div className="spinner-grow ml-1" role="status">
+                <span className="sr-only">Loading...</span>
             </div>
-            <div class="spinner-grow ml-1" role="status">
-                <span class="sr-only">Loading...</span>
+            <div className="spinner-grow ml-1" role="status">
+                <span className="sr-only">Loading...</span>
             </div>
-            <div class="spinner-grow ml-1" role="status">
-                <span class="sr-only">Loading...</span>
+            <div className="spinner-grow ml-1" role="status">
+                <span className="sr-only">Loading...</span>
             </div>
         </div>;
     //Displays States insect species is located
     const locationUI = (insect) => {
         return(
-            <div class="location-UI">
+            <div className="location-UI">
                 <i>Located: </i>
                 {insect.subNations[0].map((nation, i) => {
                     if(i + 1 === insect.subNations[0].length){
@@ -171,16 +171,16 @@ const InsectsInArea = ({ search, insects, submit, USState, loading, NatureServeC
 
 	return (
     <div style={{marginTop: 70 + "px", width: 100 + "%"}}>
-        <div class="alert alert-info alert-dismissible fade show mt-2 ml-xl-4 mr-xl-4 text-center order-list-alert mb-2" role="alert"><strong>Want to know what local insects are in your area? </strong> Select your state to see what your window screens keep out!
-          <button class="close" type="button" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;       </span></button>
+        <div className="alert alert-info alert-dismissible fade show mt-2 ml-xl-4 mr-xl-4 text-center order-list-alert mb-2" role="alert"><strong>Want to know what local insects are in your area? </strong> Select your state to see what your window screens keep out!
+          <button className="close" type="button" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;       </span></button>
         </div>
         <div style={{backgroundColor: "silver", color: 'darkslategray', height: 100 +"px"}}>
-            <div class="row m-0 p-0">
-                <h1 class="col-6 d-none d-md-block m-0 ml-3 mt-2 insect-title"><strong>Insects in <i>Your</i> Area</strong></h1>
-                <form class="col-md-5" method="post">
-                    <div class="row">
-                        <label class="col-3 mt-3 d-none d-xl-block" for="state">Select your state:</label>
-                        <select  class="form-control form-control-lg mt-3 ml-3 mr-3 col-md-8" id="state" name="state" onChange={submit.bind(this)}>
+            <div className="row m-0 p-0">
+                <h1 className="col-6 d-none d-md-block m-0 ml-3 mt-2 insect-title"><strong>Insects in <i>Your</i> Area</strong></h1>
+                <form className="col-md-5" method="post">
+                    <div className="row">
+                        <label className="col-3 mt-3 d-none d-xl-block" htmlFor="state">Select your state:</label>
+                        <select  className="form-control form-control-lg mt-3 ml-3 mr-3 col-md-8" id="state" name="state" onChange={submit.bind(this)}>
                             {states.map((state, i)=>{
                                 return(
                                     <option key={i} value={state.abbreviation} >{state.name}</option>
@@ -190,51 +190,51 @@ const InsectsInArea = ({ search, insects, submit, USState, loading, NatureServeC
                     </div>
                 </form>
             </div>
-            <hr class="sfShadow" style={{borderWidth: 2 + "px", borderColor: "darkgray"}}/>
+            <hr className="sfShadow" style={{borderWidth: 2 + "px", borderColor: "darkgray"}}/>
         </div>
-		<div class="insectMain row" width="100%">
+		<div className="insectMain row" width="100%">
           {/* Maps insect objects and creates card with insect info and photos */}
 		  {loading ? loadingUI : insects.map((insect,i) => {
             return(
             <>
-              <div class="insect col-xl-3 col-lg-4" style={{color: "darkslategray", textDecoration: "none", borderWidth: 4 + 'px', borderStyle: "solid", borderRadius: 0 + "px", height: 200 + "px"}} data-toggle="modal" data-target={`#modal${i}`} >  
-                <div class="row pl-3 pr-3 pt-3">
-                    <div class="col-8 m-0 pr-0 insect-card"  style={{overflowY: "hidden"}}>  
-                        <strong class="title text-capitalize" style={{color: 'black', fontSize: 1.25 + "rem"}}>{insect.commonName ? insect.commonName : ""}</strong><br/>
+              <div className="insect col-xl-3 col-lg-4" style={{color: "darkslategray", textDecoration: "none", borderWidth: 4 + 'px', borderStyle: "solid", borderRadius: 0 + "px", height: 200 + "px"}} data-toggle="modal" data-target={`#modal${i}`} >  
+                <div className="row pl-3 pr-3 pt-3">
+                    <div className="col-8 m-0 pr-0 insect-card"  style={{overflowY: "hidden"}}>  
+                        <strong className="title text-capitalize" style={{color: 'black', fontSize: 1.25 + "rem"}}>{insect.commonName ? insect.commonName : ""}</strong><br/>
                         <i style={{color: 'darkgray'}}>{insect.sciName}</i><br/><br/>
                     </div>
-                    <div class="insectPhoto col-4 p-2">
+                    <div className="insectPhoto col-4 p-2">
                         {photoOrLogo(insect, "insectPhotoSmall")}
                     </div>
                 </div>
-                <div class="row m-0 pl-3 pr-3 pb-3">
+                <div className="row m-0 pl-3 pr-3 pb-3">
                     {locationUI(insect)}
                 </div>
               </div> 
               {/* Pop-up modal for displaying larger insect photo */}
-              <div class="modal fade" id={`modal${i}`} tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
-              <div class="modal-dialog modal-lg" role="document">
-                <div class="modal-content">
-                  <div class="modal-header">
-                    <h5 class="modal-title text-capitalize" id="exampleModalLabel">{insect.commonName}</h5>
-                    <p class="modal-title ml-3 mt-1 text-capitalize font-italic">{insect.sciName}</p>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <div className="modal fade" id={`modal${i}`} tabIndex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
+              <div className="modal-dialog modal-lg" role="document">
+                <div className="modal-content">
+                  <div className="modal-header">
+                    <h5 className="modal-title text-capitalize" id="exampleModalLabel">{insect.commonName}</h5>
+                    <p className="modal-title ml-3 mt-1 text-capitalize font-italic">{insect.sciName}</p>
+                    <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                       <span aria-hidden="true">&times;</span>
                     </button>
                   </div>
-                  <div class="modal-body d-flex justify-content-center">
+                  <div className="modal-body d-flex justify-content-center">
                     {/* <img width="100%" src={insect.photo} /> */}
                     {photoOrLogo(insect, "insectPhotoLarge")}
                   </div>
-                  <div class="modal-footer justify-content-start">
-                      <div class="row">
-                          <div class="col-4">
+                  <div className="modal-footer justify-content-start">
+                      <div className="row">
+                          <div className="col-4">
                             {locationUI(insect)}
                           </div>
-                          <div class="col-4" style={{overflowWrap: "break-word"}}>
+                          <div className="col-4" style={{overflowWrap: "break-word"}}>
                             {NatServeCit(insect)}
                           </div>
-                          <div class="col-4" style={{overflowWrap: "break-word"}}>
+                          <div className="col-4" style={{overflowWrap: "break-word"}}>
                             {FlickrCit(insect)}    
                           </div>
                       </div>
@@ -247,7 +247,7 @@ const InsectsInArea = ({ search, insects, submit, USState, loading, NatureServeC
           })}
 		</div>
         <div style={{backgroundColor: "silver", color: 'darkslategray', height: 100 +"px", paddingTop: 15 + "px"}}>
-            <hr class="sfShadowBottom  m-0" style={{borderWidth: 2 + "px", borderColor: "darkgray"}}/>
+            <hr className="sfShadowBottom  m-0" style={{borderWidth: 2 + "px", borderColor: "darkgray"}}/>
         </div>
     </div>
 	);
