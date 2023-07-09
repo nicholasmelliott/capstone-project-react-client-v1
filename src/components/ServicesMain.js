affbba240b5990a105c20b84092da5a919156db8
nicholasmelliott <30277860+nicholasmelliott@users.noreply.github.com>
2023-07-08 21:33:33 -0400

    Refactor class attribute to className for consistent usage in the codebase.

--- src/components/ServicesMain.js
+++ src/components/ServicesMain.js
@@ -2,20 +2,20 @@ import React from 'react';
 
 const ServicesMain = ({ services , loading }) => (
 	<div>
-    <div class="d-none d-md-block services-main p-4">
-        <div class="d-flex">
+    <div className="d-none d-md-block services-main p-4">
+        <div className="d-flex">
           {services.map((service)=>{
             return(
-              <div class="card col" width="18rem" style={{backgroundColor: 'rgba(255,255,255,0)'}}>
-                <div class="row p-3">
-                  <img class="col-12 card-img-top mt-2" src={service.imgSrc} alt="Card image cap" />
+              <div className="card col" width="18rem" style={{backgroundColor: 'rgba(255,255,255,0)'}}>
+                <div className="row p-3">
+                  <img className="col-12 card-img-top mt-2" src={service.imgSrc} alt="Card image cap" />
                 </div>
-               <div class="card-body pl-0 pr-0">
-                <h5 class="card-title">{service.type}</h5>
-                <p class="card-text">{service.desc}</p>
+               <div className="card-body pl-0 pr-0">
+                <h5 className="card-title">{service.type}</h5>
+                <p className="card-text">{service.desc}</p>
                </div>
-               <div class="card-footer text-muted service-card-footer">
-                 <a class="btn btn-outline-secondary font-weight-bold btn-md btn-block footer service-btn" href={`/orders#${service.href}`}>
+               <div className="card-footer text-muted service-card-footer">
+                 <a className="btn btn-outline-secondary font-weight-bold btn-md btn-block footer service-btn" href={`/orders#${service.href}`}>
                    {service.btn}
                  </a>
                </div>
@@ -24,11 +24,11 @@ const ServicesMain = ({ services , loading }) => (
           })} 
         </div>
     </div>
-    <div class="d-md-none services-small">
-      <div class="d-flex justify-content-around flex-wrap">
+    <div className="d-md-none services-small">
+      <div className="d-flex justify-content-around flex-wrap">
         {services.map((service)=>{
           return (
-            <div class="col-4 content-justify-center mt-2 service">
+            <div className="col-4 content-justify-center mt-2 service">
               <a href={`/orders#${service.href}`}>
                 <img src={service.imgSrc} alt="Card image cap" width="80px" height="auto" />
                 <p>
