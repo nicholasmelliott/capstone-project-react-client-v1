583e3eb27877943458e492f77ad8c6222684c30c
nicholasmelliott <30277860+nicholasmelliott@users.noreply.github.com>
2023-07-09 21:02:48 -0400

    Refactor InsectsInArea component to use InsectCard

---
+++ src/components/InsectCard.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+const InsectCard = ({ insect, i, photoOrLogo, locationUI, NatServeCit, FlickrCit }) => (
+    <>
+        <div className="insect col-xl-3 col-lg-4" style={{color: "darkslategray", textDecoration: "none", borderWidth: 4 + 'px', borderStyle: "solid", borderRadius: 0 + "px", height: 200 + "px"}} data-toggle="modal" data-target={`#modal${i}`} >  
+          <div className="row pl-3 pr-3 pt-3">
+              <div className="col-8 m-0 pr-0 insect-card"  style={{overflowY: "hidden"}}>  
+                  <strong className="title text-capitalize" style={{color: 'black', fontSize: 1.25 + "rem"}}>{insect.commonName ? insect.commonName : ""}</strong><br/>
+                  <i style={{color: 'darkgray'}}>{insect.sciName}</i><br/><br/>
+              </div>
+              <div className="insectPhoto col-4 p-2">
+                  {photoOrLogo(insect, "insectPhotoSmall")}
+              </div>
+          </div>
+          <div className="row m-0 pl-3 pr-3 pb-3">
+              {locationUI(insect)}
+          </div>
+        </div> 
+        {/* Pop-up modal for displaying larger insect photo */}
+        <div className="modal fade" id={`modal${i}`} tabIndex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
+            <div className="modal-dialog modal-lg" role="document">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h5 className="modal-title text-capitalize" id="exampleModalLabel">{insect.commonName}</h5>
+                  <p className="modal-title ml-3 mt-1 text-capitalize font-italic">{insect.sciName}</p>
+                  <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div className="modal-body d-flex justify-content-center">
+                  {/* <img width="100%" src={insect.photo} /> */}
+                  {photoOrLogo(insect, "insectPhotoLarge")}
+                </div>
+                <div className="modal-footer justify-content-start">
+                    <div className="row">
+                        <div className="col-4">
+                          {locationUI(insect)}
+                        </div>
+                        <div className="col-4" style={{overflowWrap: "break-word"}}>
+                          {NatServeCit(insect)}
+                        </div>
+                        <div className="col-4" style={{overflowWrap: "break-word"}}>
+                          {FlickrCit(insect)}    
+                        </div>
+                    </div>
+                </div>
+              </div>
+            </div> 
+        </div> 
+    </>
+);
+
+export default InsectCard;
+
+
--- src/components/InsectsInArea.js
+++ src/components/InsectsInArea.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import StateSelect from './StateSelect';
+import InsectCard from './InsectCard'
 import { states } from '../data/states';
 import BeeLogo from '../insectIMGs/bee-silo.svg';
 import SpiderLogo from '../insectIMGs/spider-arthropod-animal-silhouette-1.svg';
@@ -134,54 +135,8 @@ const InsectsInArea = ({ search, insects, submit, USState, loading, NatureServeC
           {/* Maps insect objects and creates card with insect info and photos */}
 		  {loading ? loadingUI : insects.map((insect,i) => {
             return(
-            <>
-              <div className="insect col-xl-3 col-lg-4" style={{color: "darkslategray", textDecoration: "none", borderWidth: 4 + 'px', borderStyle: "solid", borderRadius: 0 + "px", height: 200 + "px"}} data-toggle="modal" data-target={`#modal${i}`} >  
-                <div className="row pl-3 pr-3 pt-3">
-                    <div className="col-8 m-0 pr-0 insect-card"  style={{overflowY: "hidden"}}>  
-                        <strong className="title text-capitalize" style={{color: 'black', fontSize: 1.25 + "rem"}}>{insect.commonName ? insect.commonName : ""}</strong><br/>
-                        <i style={{color: 'darkgray'}}>{insect.sciName}</i><br/><br/>
-                    </div>
-                    <div className="insectPhoto col-4 p-2">
-                        {photoOrLogo(insect, "insectPhotoSmall")}
-                    </div>
-                </div>
-                <div className="row m-0 pl-3 pr-3 pb-3">
-                    {locationUI(insect)}
-                </div>
-              </div> 
-              {/* Pop-up modal for displaying larger insect photo */}
-              <div className="modal fade" id={`modal${i}`} tabIndex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
-              <div className="modal-dialog modal-lg" role="document">
-                <div className="modal-content">
-                  <div className="modal-header">
-                    <h5 className="modal-title text-capitalize" id="exampleModalLabel">{insect.commonName}</h5>
-                    <p className="modal-title ml-3 mt-1 text-capitalize font-italic">{insect.sciName}</p>
-                    <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                      <span aria-hidden="true">&times;</span>
-                    </button>
-                  </div>
-                  <div className="modal-body d-flex justify-content-center">
-                    {/* <img width="100%" src={insect.photo} /> */}
-                    {photoOrLogo(insect, "insectPhotoLarge")}
-                  </div>
-                  <div className="modal-footer justify-content-start">
-                      <div className="row">
-                          <div className="col-4">
-                            {locationUI(insect)}
-                          </div>
-                          <div className="col-4" style={{overflowWrap: "break-word"}}>
-                            {NatServeCit(insect)}
-                          </div>
-                          <div className="col-4" style={{overflowWrap: "break-word"}}>
-                            {FlickrCit(insect)}    
-                          </div>
-                      </div>
-                  </div>
-                </div>
-              </div> 
-            </div> 
-            </>
-            );
+            <InsectCard key={i} insect={insect} i={i} photoOrLogo={photoOrLogo} locationUI={locationUI} NatServeCit={NatServeCit} FlickrCit={FlickrCit}/>
+            )
           })}
 		</div>
         <div style={{backgroundColor: "silver", color: 'darkslategray', height: 100 +"px", paddingTop: 15 + "px"}}>
