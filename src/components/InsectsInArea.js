b6542d4587def2168935b3a8abb164114e09674f
nicholasmelliott <30277860+nicholasmelliott@users.noreply.github.com>
2023-07-09 21:26:16 -0400

    Refactor spacing and add comment about NatSerCit function.

--- src/components/InsectsInArea.js
+++ src/components/InsectsInArea.js
@@ -21,11 +21,14 @@ const InsectsInArea = ({ search, insects, submit, USState, loading, NatureServeC
             <p style={{fontSize: 8 + "px"}}><strong>Photo Source: </strong><a href={insect.photo}>{insect.photo}</a></p>
         );
     } 
+
+    //Nature Serve citation for insect information
     const NatServeCit = (insect) =>{
         return(
             <p style={{fontSize: 8 + "px"}}><strong>Info Source: </strong><a href={`https://explorer.natureserve.org${insect.url}`}>https://explorer.natureserve.org{insect.url}</a></p>
         );
     } 
+
     //Determines whether insect object has photo to display and if not tries to display relevent logo instead
     const photoOrLogo = (insect, size) => {
         console.log(insect.informalTax);
@@ -85,6 +88,7 @@ const InsectsInArea = ({ search, insects, submit, USState, loading, NatureServeC
             }
         }
     };
+
     //Displays loading screen
     const loadingUI = 
         <div className="d-flex justify-content-center pt-3" style={{backgroundColor: "white", width: 100 + "%", height: 100 + "px"}}>
@@ -99,6 +103,7 @@ const InsectsInArea = ({ search, insects, submit, USState, loading, NatureServeC
                 <span className="sr-only">Loading...</span>
             </div>
         </div>;
+        
     //Displays States insect species is located
     const locationUI = (insect) => {
         return(
@@ -120,29 +125,30 @@ const InsectsInArea = ({ search, insects, submit, USState, loading, NatureServeC
     };
 
 	return (
-    <div style={{marginTop: 70 + "px", width: 100 + "%"}}>
-        <div className="alert alert-info alert-dismissible fade show mt-2 ml-xl-4 mr-xl-4 text-center order-list-alert mb-2" role="alert"><strong>Want to know what local insects are in your area? </strong> Select your state to see what your window screens keep out!
-          <button className="close" type="button" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;       </span></button>
-        </div>
-        <div style={{backgroundColor: "silver", color: 'darkslategray', height: 100 +"px"}}>
-            <div className="row m-0 p-0">
-                <h1 className="col-6 d-none d-md-block m-0 ml-3 mt-2 insect-title"><strong>Insects in <i>Your</i> Area</strong></h1>
-                <StateSelect submit={submit.bind(this)} states={states} className={"col-md-5"} />
+        <div style={{marginTop: 70 + "px", width: 100 + "%"}}>
+            <div className="alert alert-info alert-dismissible fade show mt-2 ml-xl-4 mr-xl-4 text-center order-list-alert mb-2" role="alert">
+                <strong>Want to know what local insects are in your area? </strong> Select your state to see what your window screens keep out!
+                <button className="close" type="button" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;       </span></button>
             </div>
-            <hr className="sfShadow" style={{borderWidth: 2 + "px", borderColor: "darkgray"}}/>
+            <div style={{backgroundColor: "silver", color: 'darkslategray', height: 100 +"px"}}>
+                <div className="row m-0 p-0">
+                    <h1 className="col-6 d-none d-md-block m-0 ml-3 mt-2 insect-title"><strong>Insects in <i>Your</i> Area</strong></h1>
+                    <StateSelect submit={submit.bind(this)} states={states} className={"col-md-5"} />
+                </div>
+                <hr className="sfShadow" style={{borderWidth: 2 + "px", borderColor: "darkgray"}}/>
+            </div>
+	        <div className="insectMain row" width="100%">
+                {/* Maps insect objects and creates card with insect info and photos */}
+	            {loading ? loadingUI : insects.map((insect,i) => {
+                    return(
+                        <InsectCard key={i} insect={insect} i={i} photoOrLogo={photoOrLogo} locationUI={locationUI} NatServeCit={NatServeCit} FlickrCit={FlickrCit}/>
+                    );
+                })}
+	        </div>
+            <div style={{backgroundColor: "silver", color: 'darkslategray', height: 100 +"px", paddingTop: 15 + "px"}}>
+                <hr className="sfShadowBottom  m-0" style={{borderWidth: 2 + "px", borderColor: "darkgray"}}/>
+            </div>
         </div>
-		<div className="insectMain row" width="100%">
-          {/* Maps insect objects and creates card with insect info and photos */}
-		  {loading ? loadingUI : insects.map((insect,i) => {
-            return(
-            <InsectCard key={i} insect={insect} i={i} photoOrLogo={photoOrLogo} locationUI={locationUI} NatServeCit={NatServeCit} FlickrCit={FlickrCit}/>
-            )
-          })}
-		</div>
-        <div style={{backgroundColor: "silver", color: 'darkslategray', height: 100 +"px", paddingTop: 15 + "px"}}>
-            <hr className="sfShadowBottom  m-0" style={{borderWidth: 2 + "px", borderColor: "darkgray"}}/>
-        </div>
-    </div>
 	);
 };
 
