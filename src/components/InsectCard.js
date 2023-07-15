137025579392306b1f3db2e4bfaa89d46315db0b
nicholasmelliott <30277860+nicholasmelliott@users.noreply.github.com>
2023-07-15 10:04:29 -0400

    Refactor inline styling into style.css. Refactor insectPhoto className into insect-photo. Reorder custom styling to end of className attribute

--- src/components/InsectCard.js
+++ src/components/InsectCard.js
@@ -4,13 +4,13 @@ import InsectLocations from './InsectLocations';
 
 const InsectCard = ({ insect, i }) => (
     <>
-        <div className="insect col-xl-3 col-lg-4" style={{color: "darkslategray", textDecoration: "none", borderWidth: 4 + 'px', borderStyle: "solid", borderRadius: 0 + "px", height: 200 + "px"}} data-toggle="modal" data-target={`#modal${i}`} >  
+        <div className="col-xl-3 col-lg-4 insect " data-toggle="modal" data-target={`#modal${i}`} >  
           <div className="row pl-3 pr-3 pt-3">
-              <div className="col-8 m-0 pr-0 insect-card"  style={{overflowY: "hidden"}}>  
-                  <strong className="title text-capitalize" style={{color: 'black', fontSize: 1.25 + "rem"}}>{insect.commonName ? insect.commonName : ""}</strong><br/>
-                  <i style={{color: 'darkgray'}}>{insect.sciName}</i><br/><br/>
+              <div className="col-8 m-0 pr-0 insect-card">  
+                  <strong className="text-capitalize title">{insect.commonName ? insect.commonName : ""}</strong><br/>
+                  <i className="sub-title">{insect.sciName}</i><br/><br/>
               </div>
-              <div className="insectPhoto col-4 p-2">
+              <div className="col-4 p-2 insect-photo ">
                 <InsectImage insect={insect} size={"insectPhotoSmall"} />
               </div>
           </div>
