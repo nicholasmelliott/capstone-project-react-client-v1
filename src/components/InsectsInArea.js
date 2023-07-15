c9cec28ca70caa0490e1731e5549156f97903cef
nicholasmelliott <30277860+nicholasmelliott@users.noreply.github.com>
2023-07-15 16:06:06 -0400

    Refactor inline styling into style.css

--- src/components/InsectsInArea.js
+++ src/components/InsectsInArea.js
@@ -8,17 +8,19 @@ import { states } from '../data/states';
 
 const InsectsInArea = ({ search, insects, submit, USState, loading, NatureServeCit}) => { 
 	return (
-        <div style={{marginTop: 70 + "px", width: 100 + "%"}}>     
+        <div className="insect-main-wrapper">     
             <CustomAlert
               title="Want to know what local insects are in your area?"
               message="Select your state to see what your window screens keep out!"
             />
-            <div style={{backgroundColor: "silver", color: 'darkslategray', height: 100 +"px"}}>
+            <div className="screen-frame-top">
                 <div className="row m-0 p-0">
-                    <h1 className="col-6 d-none d-md-block m-0 ml-3 mt-2 insect-title"><strong>Insects in <i>Your</i> Area</strong></h1>
+                    <h1 className="col-6 d-none d-md-block m-0 ml-3 mt-2 insect-title">
+                        <strong>Insects in <i>Your</i> Area</strong>
+                    </h1>
                     <StateSelect submit={submit.bind(this)} states={states} className={"col-md-5"} />
                 </div>
-                <hr className="sfShadow" style={{borderWidth: 2 + "px", borderColor: "darkgray"}}/>
+                <hr className="sfShadow"/>
             </div>
 	        <div className="row m-0">
                 {/* Maps insect objects and creates card with insect info and photos */}
@@ -33,8 +35,8 @@ const InsectsInArea = ({ search, insects, submit, USState, loading, NatureServeC
                         ))
                 }
 	        </div>
-            <div style={{backgroundColor: "silver", color: 'darkslategray', height: 100 +"px", paddingTop: 15 + "px"}}>
-                <hr className="sfShadowBottom  m-0" style={{borderWidth: 2 + "px", borderColor: "darkgray"}}/>
+            <div className="screen-frame-bottom">
+                <hr className="sfShadowBottom  m-0"/>
             </div>
         </div>
 	);
